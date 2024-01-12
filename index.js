4a822cc64fa237b2e84d2a52e384faf989742f70
Edmond Prin <114427318+edmondprin@users.noreply.github.com>
2024-01-11 22:39:56 -0600

    Update index.js
    Added do-while loop for number of clockIn clockOut cycles

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ let temp2 = 0;
 
 function shift() {
   let date = prompt("Enter date (format 25 Feb 2024): ");
-  let numberShifts = Number(prompt("How many times did you clock in/out that day? "));
+  
+  let numberShifts;
+  do {
+    numberShifts = Number(prompt("How many times did you clock in/out that day? "));
+  } while (isNaN(numberShifts));
+  
   for (let i = 0; i < numberShifts; i++) {
     let clockIn = new Date(date + " " + prompt(`Clockin number ${i + 1} | Enter the date and time you clocked in: `));
     let clockOut = new Date(date + " " + prompt(`Clockout number ${i + 1} | Enter the date and time you clocked out: `));
     //let diff = Date.parse(parseInt(clockOut)) - Date.parse(parseInt(clockIn));
     
     let diff = clockOut.getTime() - clockIn.getTime()
-    console.log("clockIn: ", clockIn, "clockOut: ", clockOut);
-    console.log(diff);
+    //console.log("clockIn: ", clockIn, "clockOut: ", clockOut);
+    //console.log(diff);
     temp2 += diff;
     // temp.push(diff);
-    console.log(temp2);
+    // console.log(temp2);
   }
     let Days = Math.floor(temp2/ (1000 * 60 * 60 * 24));
     let hours = Math.floor(temp2 / (60 * 60 * 1000) % 24);
